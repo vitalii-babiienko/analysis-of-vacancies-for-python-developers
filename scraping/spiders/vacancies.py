4786118d4e1b530347d9ae475d2de03c604900ba
Vitalii Babiienko <v.babiyenko@gmail.com>
2023-12-07 11:02:31 +0200

    Create a spider the vacancies

---
+++ scraping/spiders/vacancies.py
@@ -0,0 +1,11 @@
+import scrapy
+from scrapy.http import Response
+
+
+class VacanciesSpider(scrapy.Spider):
+    name = "vacancies"
+    allowed_domains = ["djinni.co"]
+    start_urls = ["https://djinni.co/jobs/?primary_keyword=Python"]
+
+    def parse(self, response: Response, **kwargs):
+        pass
